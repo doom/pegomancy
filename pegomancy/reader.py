9f6a372e1fce7fd81b5f3100943dafc6af955b05
Doom <clement.doumergue@epitech.eu>
2020-05-26 13:37:09 +0200

    Add docstrings and skipping of non-significant characters in Reader

--- pegomancy/reader.py
+++ pegomancy/reader.py
@@ -1,5 +1,4 @@
 import re
-import string
 from typing import Optional
 
 
@@ -12,24 +11,49 @@ class Reader:
             self,
             text: str,
             *,
-            whitespace_regex: Optional[str] = None,
+            whitespace_regex: Optional[str] = r"[ \t]+",
             comments_regex: Optional[str] = None,
     ):
+        """
+        :param text:                the text to process
+        :param whitespace_regex:    the regex pattern to use to match whitespace, or None for no whitespace support
+        :param comments_regex:      the regex pattern to use to match comments, or None for no comments support
+        """
         self.whitespace_regex = whitespace_regex
         self.comments_regex = comments_regex
         self.text = text
         self.cursor = 0
 
     def debug(self, lookahead: int = 10):
+        """
+        Debug the current state of the reader
+
+        :param lookahead:           the number of characters to consider when printing
+        """
         print(f"DEBUG: cursor: {self.cursor}, text: {self.text[self.cursor:self.cursor + lookahead]!r}")
 
     def eof(self) -> bool:
+        """
+        Check whether the reader has reached the end of the source text
+
+        :return:                    True if the reader has reached the end of the source text, False otherwise
+        """
         return self.cursor == len(self.text)
 
     def peek(self):
+        """
+        Retrieve the current character without consuming it
+
+        :return:                    the retrieved character
+        """
         return self.text[self.cursor]
 
     def get(self):
+        """
+        Retrieve the current character and consume it
+
+        :return:                    the retrieved character
+        """
         c = self.peek()
         self.advance(1)
         return c
@@ -38,8 +62,8 @@ class Reader:
         """
         Match text with a regex pattern and consume it
 
-        :param regex:       the pattern to match with
-        :return:            the consumed text
+        :param regex:               the pattern to match with
+        :return:                    the consumed text
         """
         result = re.match(regex, self.text[self.cursor:], flags=re.DOTALL | re.MULTILINE)
         if result is None:
@@ -47,23 +71,76 @@ class Reader:
         self.advance(result.end(0))
         return result.group(0)
 
-    def expect_string(self, literal: str, allow_partial: bool = False):
+    def expect_string(self, literal: str, match_full_token: bool = True):
+        """
+        Match text with a literal string and consume it
+
+        :param literal:             the literal to match with
+        :param match_full_token:    whether or not the match must consume a full token
+        :return:                    the consumed text
+        """
         pos = self.mark()
         if self.text[self.cursor:].startswith(literal):
             self.advance(len(literal))
-            if allow_partial or self.eof():
+            if not match_full_token or self.eof():
                 return literal
             p = self.peek()
-            if p is None or not (p.isalnum() and all(map(str.isalnum, literal))):
+            if not (p.isalnum() and all(map(str.isalnum, literal))):
                 return literal
         self.rewind(pos)
         return None
 
-    def advance(self, offset: int):
-        return self.rewind(self.cursor + offset)
+    def consume_whitespace(self):
+        """
+        Consume whitespace as matched using the whitespace_regex pattern
 
-    def mark(self):
+        :return:                    the consumed text, or None if no match was found
+        """
+        if self.whitespace_regex is not None:
+            return self.expect_regex(self.whitespace_regex)
+        return None
+
+    def consume_comment(self):
+        """
+        Consume a comment as matched using the comment_regex pattern
+
+        :return:                    the consumed text, or None if no match was found
+        """
+        if self.comments_regex is not None:
+            return self.expect_regex(self.comments_regex)
+        return None
+
+    def consume_non_significant(self):
+        """
+        Consume non-significant text, that is comments and whitespace
+        """
+        while True:
+            pos = self.mark()
+            self.consume_comment()
+            self.consume_whitespace()
+            if pos == self.mark():
+                break
+
+    def advance(self, offset: int):
+        """
+        Move the cursor forward
+
+        :param offset:              the offset to add to the current cursor position
+        """
+        self.rewind(self.cursor + offset)
+
+    def mark(self) -> int:
+        """
+        Get the current position of the cursor
+
+        :return:                    the current position of the cursor
+        """
         return self.cursor
 
-    def rewind(self, cursor):
-        self.cursor = cursor
+    def rewind(self, position: int):
+        """
+        Rewind the cursor to an existing position
+
+        :param position:            the position at which to rewind
+        """
+        self.cursor = position
